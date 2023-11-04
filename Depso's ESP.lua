01ad6c43f41745a80e4137025f126bee3b03bc1b
LordDepso <149268507+LordDepso@users.noreply.github.com>
2023-11-04 16:33:55 +0000

    Update Depso's ESP.lua

--- Depso's ESP.lua
+++ Depso's ESP.lua
@@ -1,69 +1,123 @@
--- 19/10/23
+-- 4/11/23
+-- depso
 
 _G.DESPConfig = {
+	-------------
+	-- Display --
+	-------------
 	Default_Color = Color3.fromRGB(200,200,200),
-	Opacity = 0.9,
-	Thickness = 1,
+	Rainbow = true,
 	-------------
 	UseTeamColor = true, -- Use the team's color instead of the default
 	OtherTeamsOnly = true, -- Hide your team
 	ShowDead = true, -- Continue to hilight dead players
+	CheckVisibility = true, -- Hide if it's not visible 
+	
+	-------------
+	--  Items  --
 	-------------
 	Tracers = true,
-	Boxes = true
+	TracerThickness = 1,
+	TracerOpacity = 0.2,
+	-------------
+	Boxes = true,
+	BoxThickness = 5,
+	BoxOpacity = 0.9
 }
 
 --Drawing.clear()
 
 local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 
+local LocalPlayer = Players.LocalPlayer
 local Camera = workspace.CurrentCamera
-local mouse = LocalPlayer:GetMouse()
+local RainbowState
+
+assert(Drawing, 'Your exploit does not support the Drawing library.')
+
+if _G.DESPConfig.Rainbow then
+	RunService.RenderStepped:Connect(function()
+		local hue = tick()%5/5
+		RainbowState = Color3.fromHSV(hue,1,1)
+	end)
+end
+
+function IsVisible(Part)
+	if not Part then
+		return
+	end
+	
+	local ObstructingParts = Camera:GetPartsObscuringTarget({Part.Position}, {Part.Parent,LocalPlayer.Character})
+
+	for i,Part in next, ObstructingParts do
+		pcall(function()
+			if Part.Transparency >= 1 then
+				table.remove(ObstructingParts, i)
+			end
+		end)
+	end
+	
+	return #ObstructingParts <= 0
+end
+
+function GetColor(Player)
+	local C = _G.DESPConfig
+	return  (C.Rainbow and RainbowState) 
+		or (C.UseTeamColor and Player.TeamColor and Player.TeamColor.Color) 
+		or C.Default_Color
+end
 
 function ToScreen(p)
-	return Camera:WorldToViewportPoint(p)
+	local _, Visible = Camera:WorldToScreenPoint(p)--Camera:WorldToViewportPoint(p)
+	return Vector2.new(_.X, _.Y), Visible
 end
+
+function CheckTeam(Player)
+	return _G.DESPConfig.OtherTeamsOnly and Player.Team and Player.Team == LocalPlayer.Team
+end
+
 function WorkPositions(Character)
 	if not Character then
 		return
 	end
-	
+
 	local Center,Offsets = Character:GetBoundingBox()
 	local Humanoid = Character:FindFirstChildOfClass("Humanoid")
 	local ScreenCenter,Visible = ToScreen(Center.Position)
-	
+
 	if not _G.DESPConfig.ShowDead and (not Humanoid or Humanoid.Health < 1) or not Visible then
 		return
 	end
-	
+
 	return {
-		ToScreen((Center * CFrame.new(-Offsets.X,Offsets.Y,0)).Position),
-		ToScreen((Center * CFrame.new(Offsets.X,Offsets.Y,0)).Position),
-		ToScreen((Center * CFrame.new(Offsets.X,-Offsets.Y,0)).Position),
-		ToScreen((Center * CFrame.new(-Offsets.X,-Offsets.Y,0)).Position),
-		ScreenCenter
+		ToScreen((Center * CFrame.new(-Offsets.X/2,Offsets.Y/2,0)).Position),
+		ToScreen((Center * CFrame.new(Offsets.X/2,Offsets.Y/2,0)).Position),
+		ToScreen((Center * CFrame.new(Offsets.X/2,-Offsets.Y/2,0)).Position),
+		ToScreen((Center * CFrame.new(-Offsets.X/2,-Offsets.Y/2,0)).Position),
+		Vector2.new(ScreenCenter.X,ScreenCenter.Y)
 	}
 end
+
 function Outline(Player)
 	local Box, Tracer
 	local Lib = {}
-	
+
 	if _G.DESPConfig.Boxes then
 		Box = Drawing.new("Quad")
 		Box.Visible = true
 		Box.Color = Color3.fromRGB(255, 255, 255)
-		Box.Thickness = _G.DESPConfig.Thickness
-		Box.Transparency = _G.DESPConfig.Opacity
+		Box.Thickness = _G.DESPConfig.BoxThickness
+		Box.Transparency = _G.DESPConfig.BoxOpacity
 	end
 	if _G.DESPConfig.Tracers then
 		Tracer = Drawing.new("Line")
 		Tracer.Visible = true
 		Tracer.Color = Color3.fromRGB(255, 255, 255)
-		Tracer.Thickness = _G.DESPConfig.Thickness
-		Tracer.Transparency = _G.DESPConfig.Opacity
-		Tracer.From = Vector2.new(mouse.ViewSizeX/2,  mouse.ViewSizeY)
+		Tracer.Thickness = _G.DESPConfig.TracerThickness
+		Tracer.Transparency = _G.DESPConfig.TracerOpacity
+		Tracer.From = Vector2.new(Camera.ViewportSize.X/2,  Camera.ViewportSize.Y-30)
 		Tracer.To = Tracer.From
 	end
 
@@ -77,22 +131,29 @@ function Outline(Player)
 	end
 
 	Lib.Render = RunService.RenderStepped:Connect(function()
-		if _G.DESPConfig.OtherTeamsOnly and Player.Team and Player.Team == LocalPlayer.Team then
+		local Character = Player.Character
+		if not Character then
+			return
+		end
+		if CheckTeam(Player) then
 			return Lib.SetVisible(false)
 		end
+		if _G.DESPConfig.CheckVisibility and not IsVisible(Character.PrimaryPart or Character:FindFirstChildOfClass("Part")) then
+			return Lib.SetVisible(false)
+		end
+		
 		local Points = WorkPositions(Player.Character)
 		if not Points then
 			return Lib.SetVisible(false)
 		end	
 		Lib.SetVisible(true)
 
-		local Color = (_G.DESPConfig.UseTeamColor and Player.TeamColor and Player.TeamColor.Color) or _G.DESPConfig.Default_Color
+		local Color = GetColor(Player)
 		if Box then
 			Box.PointA = Points[1]
 			Box.PointB = Points[2]
 			Box.PointC = Points[3]
 			Box.PointD = Points[4]
-			Box.Position = Points[5]
 			Box.Color = Color
 		end
 		if Tracer then
@@ -117,7 +178,7 @@ function Outline(Player)
 end
 
 
-for _, Player in next, Players:GetPlayers() do
+for _, Player in next, Players:GetChildren() do
 	if Player ~= LocalPlayer then
 		pcall(spawn, function()
 			Outline(Player)
@@ -125,3 +186,13 @@ for _, Player in next, Players:GetPlayers() do
 	end
 end
 Players.PlayerAdded:Connect(Outline)
+
+--[[
+for _, Coin in next, workspace.Coins:GetChildren() do
+	pcall(spawn, function()
+		Outline({
+			Character = Coin
+		})
+	end)
+end
+]]
